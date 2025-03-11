77a1926487ebb77a6631e01f297257a2d2f8a155
nirali <nirali@karmadhi.com>
2025-03-11 18:12:35 +0530

    header reponsive

--- resources/assets/scripts/parts/header.js
+++ resources/assets/scripts/parts/header.js
@@ -1,6 +1,7 @@
 export class Header {
     init() {
         this.HeaderFixed();
+        this.HeaderResponsive();
     }
     HeaderFixed() {
         // header fixed js
@@ -23,5 +24,28 @@ export class Header {
 			}
 			prevScrollPos = currentScrollPos;
 		});
+    }
+    HeaderResponsive(){
+        $(document).ready(function(){
+            $('.menu-icon').click(function(){
+                if(!$(this).hasClass('close-header')){
+                    $(this).addClass('close-header');
+                    $('body').addClass('overflow-hidden');
+                    $('.header').addClass('header-open');
+                }else{
+                    $(this).removeClass('close-header');
+                    $('body').removeClass('overflow-hidden');
+                    $('.header').removeClass('header-open');
+                }
+            })
+            $('.sub-menu-link').click(function(){
+                let sub_menu = $(this).find('.sub-menu')
+                if(!sub_menu.hasClass('active-sub-menu')){
+                    sub_menu.addClass('active-sub-menu');
+                }else{
+                    sub_menu.removeClass('active-sub-menu');
+                }
+            })
+        })
     }
 }
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-    <!-- <?php
+    <?php
     $header_logo = get_field('header_logo', 'option');
     $white_logo = get_field('white_logo', 'option');
     $black_logo = get_field('black_logo', 'option');
@@ -7,10 +7,10 @@
     $get_started_button = get_field('get_started_button', 'option');
     $modal_button_text = get_field('modal_button_text', 'option');
     ?>
-    <header class="dpt-30 dpb-30 header position-fixed w-100 top-0">
+    <header class="dpt-30 dpb-30 header position-fixed w-100 top-0 tpt-20 tpb-20">
         <div class="container-large">
             <div class="row align-items-center justify-content-between">
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-4">
                     <div class="header-logo">
                         <a href="<?php echo get_home_url(); ?>" class="h-100">
                             <?php if (!empty($white_logo)): ?>
@@ -22,7 +22,7 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-6 nav-container d-lg-flex d-none">
                     <nav class="header-container">
                         <?php if (!empty($nav_links)): ?>
                             <ul class="d-flex header-menu w-fit mx-auto align-items-center mb-0 p-0 list-none logo--bg rounded-pill px-2 justify-content-center">
@@ -62,7 +62,84 @@
                         <?php endif; ?>
                     </nav>
                 </div>
-                <div class="col-lg-3 text-end">
+                <div class="col-lg-3 text-end d-lg-flex d-none">
+                    <?php if ($link_or_modal == 'link'): ?>
+                        <?php if (!empty($get_started_button['url'])): ?>
+                            <a href="<?php echo $get_started_button['url'] ?>" class="theme-btn transition text--color white-btn urbanist font20 rounded-pill text-decoration-none">
+                                <span class="btn-text"><?php echo $get_started_button['title'] ?></span>
+                                <span class="arrow">
+                                    <svg width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="transition">
+                                        <path
+                                            class="transition"
+                                            d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM26.7071 8.70711C27.0976 8.31658 27.0976 7.68342 26.7071 7.29289L20.3431 0.928932C19.9526 0.538408 19.3195 0.538408 18.9289 0.928932C18.5384 1.31946 18.5384 1.95262 18.9289 2.34315L24.5858 8L18.9289 13.6569C18.5384 14.0474 18.5384 14.6805 18.9289 15.0711C19.3195 15.4616 19.9526 15.4616 20.3431 15.0711L26.7071 8.70711ZM1 9H26V7H1V9Z"
+                                            fill="white" />
+                                    </svg>
+                                </span>
+                            </a>
+                        <?php endif; ?>
+                    <?php else: ?>
+                        <?php if (!empty($modal_button_text)): ?>
+                            <a href="#FreeQuote" class="theme-btn transition text--color text-decoration-none white-btn urbanist font20 rounded-pill" data-bs-toggle="modal">
+                                <span class="btn-text"><?= $modal_button_text; ?></span>
+                                <span class="arrow">
+                                    <svg width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                        class="transition">
+                                        <path class="transition"
+                                            d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM26.7071 8.70711C27.0976 8.31658 27.0976 7.68342 26.7071 7.29289L20.3431 0.928932C19.9526 0.538408 19.3195 0.538408 18.9289 0.928932C18.5384 1.31946 18.5384 1.95262 18.9289 2.34315L24.5858 8L18.9289 13.6569C18.5384 14.0474 18.5384 14.6805 18.9289 15.0711C19.3195 15.4616 19.9526 15.4616 20.3431 15.0711L26.7071 8.70711ZM1 9H26V7H1V9Z"
+                                            fill="white" />
+                                    </svg>
+                                </span>
+                            </a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col-4 d-lg-none d-flex justify-content-end">
+                    <div class="menu-icon cursor-pointer">
+                        <img src="/wp-content/uploads/2025/03/menu.png" alt="menu-icon" class="h-100 object-fit-contain menu-bar">
+                        <img src="/wp-content/uploads/2025/03/close.png" alt="menu-icon" class="h-100 object-fit-contain close-bar">
+                    </div>
+                </div>
+            </div>
+            <div class="logo--bg mobile-header transition px-2">
+                <nav class="header-container pt-2">
+                    <?php if (!empty($nav_links)): ?>
+                        <ul class="header-menu w-fit mb-0 p-0 list-none">
+                            <?php foreach ($nav_links as $nav_link) : ?>
+                                <?php if (!empty($nav_link['sub_link'])): ?>
+                                    <?php if (!empty($nav_link['link']['url'])): ?>
+                                        <li class="menu-link sub-menu-link">
+                                            <a href="<?php echo $nav_link['link']['url'] ?>" class="text-decoration-none text--color font20 urbanist leading1_2 d-flex">
+                                                <?php echo $nav_link['link']['title'] ?>
+                                                <span class="sub-arrow">
+                                                    <img src="<?= get_template_directory_uri(); ?>/assets/images/down-arrow.svg" alt="" class="h-100 w-100 object-fit-contain" />
+                                                </span>
+                                            </a>
+                                            <div class="sub-menu">
+                                                <ul class="mb-0 ps-0 list-none">
+                                                    <?php if (!empty($nav_link['sub_link'])): ?>
+                                                        <?php foreach ($nav_link['sub_link'] as $sub_link) : ?>
+                                                            <?php if (!empty($sub_link['link']['url'])): ?>
+                                                                <li><a href="<?php echo $sub_link['link']['url'] ?>" class="text--color text-decoration-none font20 urbanist leading1_2"><?php echo $sub_link['link']['title'] ?></a></li>
+                                                            <?php endif; ?>
+                                                        <?php endforeach; ?>
+                                                    <?php endif; ?>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                    <?php endif; ?>
+                                <?php else: ?>
+                                    <?php if (!empty($nav_link['link']['url'])): ?>
+                                        <li class="menu-link">
+                                            <a href="<?php echo $nav_link['link']['url'] ?>" class="text-decoration-none text--color font20 urbanist leading1_2"><?php echo $nav_link['link']['title'] ?></a>
+                                        </li>
+                                    <?php endif; ?>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+
+                        </ul>
+                    <?php endif; ?>
+                </nav>
+                <div class="px-3 pb-3">
                     <?php if ($link_or_modal == 'link'): ?>
                         <?php if (!empty($get_started_button['url'])): ?>
                             <a href="<?php echo $get_started_button['url'] ?>" class="theme-btn transition text--color white-btn urbanist font20 rounded-pill text-decoration-none">
@@ -95,4 +172,4 @@
                 </div>
             </div>
         </div>
-    </header> -->
+    </header>
